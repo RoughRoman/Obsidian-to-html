a623d9a1ddd15c7d985544e0b1c9cf8a7c482e14
Matthew Lategan <likewise52@gmail.com>
2023-12-05 15:08:08 +0200

    Changing app to use md-block html element instead of converting. Much simpler and faster.

--- converter.py
+++ converter.py
@@ -1,6 +1,6 @@
 import os
 from multiprocessing import Pool
-from markdown2 import markdown_path
+from shutil import copy
 
 class Converter:
     def __init__(self, vault_path, dest_folder):
@@ -17,16 +17,27 @@ class Converter:
     def convert(self, parallel):
         if parallel:
             # process in sub processes
+            
             with Pool() as p:
-                p.map(markdown_path, self.md_files)
+                html_strs = p.map(format, self.md_files)
+
+            for str in html_strs:
+
+
             
         else:
             print("single")
             # process with single cpu
             for md_file in self.md_files:
-                print(markdown_path(md_file))
-            
+                print(format(md_file))
 
+        # copy images into dest folder in its own subfolder
+        image_dest_folder = os.path.join(self.dest_folder, "Images")
+        os.mkdir(image_dest_folder)
+
+        for img_file in self.images:
+            copy(img_file, image_dest_folder, True)
+        
     def traverse(self):
         for root, dirs, files in os.walk(self.vault_path):
             for file in files:
@@ -34,3 +45,26 @@ class Converter:
                      self.md_files.append(os.path.join(root, file))
                 elif file.endswith(".png"):
                     self.images.append(os.path.join(root, file))
+
+
+    def format(self, md_filePath):
+        md_file = open(md_filePath,"r")
+        md_str = md_file.read()
+        md_file.close()
+
+        html_file = (
+            f'<!DOCTYPE html>'+ '\n'+
+            '<html lang="en">'+ '\n'+
+                '<head>'+ '\n'+
+                    '<meta charset="UTF-8">'+ '\n'+
+                    '<meta name="viewport" content="width=device-width, initial-scale=1.0">'+ '\n'+
+                    '<meta http-equiv="X-UA-Compatible" content="ie=edge">'+ '\n'+
+                    '<title>{title}</title>'+ '\n'+
+                    '<link rel="stylesheet" href="style.css">'+ '\n'+
+                '</head>'+ '\n'+
+                '<body>'+ '\n'+
+                    '<script src="index.js"></script>'+ '\n'+
+                    
+                '</body>'+ '\n'+
+            '</html>'
+            ) 
--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from mainWindow import MainWindow
 
 if __name__== "__main__":
     app = MainWindow() 
+
+# Notes We now using md-block, a custom html element that renders
+# markdown. Just download the js file
+# from https://md-block.verou.me/md-block.js
+# The idea will be. Python will download that js script. place it
+# into the directory with the html files so that the html can find it
+# then simply write the md file content into an html template with
+# the md-block element in it. 
