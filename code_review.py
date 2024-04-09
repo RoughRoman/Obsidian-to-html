3aec01e0e64a4d8066e4758e88c2097316d6caa6
matthew <likewise52@gmail.com>
2024-04-09 10:20:51 +0200

    Reset

--- code_review.py
+++ code_review.py
@@ -1,6 +1,7 @@
 import sys, os
 from openai import OpenAI
 import requests
+import json
 
 code = sys.stdin.read()
 
@@ -10,14 +11,20 @@ client = OpenAI(
 
 email = os.getenv('EMAIL')
 name = os.getenv('NAME')
+lang = os.getenv('LANG')
+user_model = os.getenv('MODEL')
+custom_instructions = os.getenv('CUSTOM_INSTRUCTIONS')
+callback_url = os.getenv('CALLBACK')
 
-response = client.completions.create(
-    model="gpt-3.5-turbo",
-    messages= [{"role":"user", "content": f"Please Review the following Pull request from {name} ({email}) containing the following code changes:\n```{code}```"}]
+response = client.chat.completions.create(
+  model=user_model,
+  messages=[
+    {"role": "system", "content": f"You are an experienced {lang} Programmer who will review the following code. Look for potential errors and bad style for {lang} as well as any custom standards supplied before the code. Do not rewrite any code. Supply a list of recommended changes in plain english. Also supply a short summary of what the code does. "},
+    {"role": "user", "content": f"Here are the standards if supplied: {custom_instructions}. Here is the {code}"}
+  ]
 )
 
-api_url = 'https://prod-66.westeurope.logic.azure.com:443/workflows/867f215910024d3a8d8623eb0b4dc8e4/triggers/manual/paths/invoke?api-version=2016-06-01'
+payload = {"email":f"{email}","name":f"{name}","message":f"{str(response.choices[0].message)}"}
+response = requests.post(callback_url, str(payload))
 
-response = requests.post(api_url, data=response)
 
-print(response.text)
